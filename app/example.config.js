8b8bc24538aa5de4cea9bd1409fdadde63e5d877
Will Washburn <will@alfred>
2013-03-08 17:00:50 -0500

    add instagram config options

--- app/example.config.js
+++ app/example.config.js
@@ -13,7 +13,8 @@ module.exports = {
 		client_id: "you'd get this from ig",
         client_secret: "you'd also get this from ig",
         user_id: "4810557",
-        access_token: "I stole this from the apigee console to cut a corner so sue me"
+        access_token: "I stole this from the apigee console to cut a corner so sue me"a,
+        api:"https://api.instagram.com/v1/"
 	},
     tumblr: {
         blog: "blog.willwashburn.com",
