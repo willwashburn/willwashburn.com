50e3052599d75d11cfbc267e38f89a84946d8c15
Will Washburn <will@alfred>
2013-03-10 04:09:51 -0400

    complete skills data

--- app/data.js
+++ app/data.js
@@ -28,10 +28,45 @@ module.exports = {
         }, {
             skill: 'Node.js',
             level: 1
-        }],
-        "green": [{
+        }, {
+            skill: 'MySQL',
+            level: 3
+        }, {
+            skill: 'MongoDB',
+            level: 1
+        }, {
+            skill: 'Express',
+            level: 1
+        }, {
             skill: 'Apache',
             level: 2
+        }, {
+            skill: 'Git',
+            level: 2
+        }
+
+        ],
+        "green": [{
+            skill: 'Adobe Fireworks',
+            level: 2
+        }, {
+            skill: 'Adobe Photoshop',
+            level: 1
+        }, {
+            skill: 'PhoneGap / Cordova',
+            level: 2
+        }, {
+            skill: 'Objective-C',
+            level: 1
+        }, {
+            skill: 'Hackathons',
+            level: 5
+        }, {
+            skill: 'Basketballin',
+            level: 5
+        }, {
+            skill: 'Git Puns',
+            level: 99
         }]
     }
 
