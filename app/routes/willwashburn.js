5654dd5328df305f7234219dc8d8650290834a4f
Will Washburn <will@alfred>
2013-03-12 01:51:12 -0400

    instagram flowin inn

--- app/routes/willwashburn.js
+++ app/routes/willwashburn.js
@@ -19,55 +19,97 @@ module.exports = function(app) {
         response.skills = skills;
 
         request({
-            uri: config.instagram.api + 'users/' + config.instagram.user_id + '/media/recent?access_token=' + config.instagram.access_token
+            uri: config.instagram.api + 'users/' + config.instagram.user_id + '/media/recent?count=10&access_token=' + config.instagram.access_token
         }, function(error, response, body) {
 
             if (!error && response.statusCode == 200) {
 
 
-                response.photos = JSON.parse(body);
+                var photo_feed = JSON.parse(body);
 
-                var T = new Twit({
-                    consumer_key: config.twitter.consumer_key,
-                    consumer_secret: config.twitter.consumer_secret,
-                    access_token: config.twitter.access_token,
-                    access_token_secret: config.twitter.access_token_secret
+
+                photos = [];
+
+                _.each(photo_feed.data, function(ig) {
+                    var photo = {};
+
+                    photo.img_src = ig.images.thumbnail.url;
+
+                    photos.push(photo);
                 });
 
+                var blank_photo = {
+                    "img_src": false
+                };
 
-                T.get('statuses/user_timeline', {
-                    screen_name: config.screen_name,
-                    count: 3
-                }, function(err, reply) {
+                var photo_set_1 = photos.splice(0, 4);
+                photos.unshift(blank_photo);
+                var photo_set_2 = photos.splice(0, 4);
+                photos.unshift(blank_photo, blank_photo);
+                var photo_set_3 = photos.splice(0, 4);
+                photos.unshift(blank_photo,blank_photo,blank_photo);
 
-                    var tweets = [];
-                    _.each(reply, function(raw_tweet) {
-                        var tweet = {};
-                        if (raw_tweet.retweeted_status) {
-                            tweet.img = raw_tweet.retweeted_status.user.profile_image_url;
-                            tweet.username = raw_tweet.retweeted_status.user.screen_name;
-                            tweet.tweet = raw_tweet.retweeted_status.text;
-                            tweet.retweeted = true;
-                        } else {
-                            tweet.username = raw_tweet.user.screen_name;
-                            tweet.img = raw_tweet.user.profile_image_url;
-                            tweet.tweet = raw_tweet.text;
-                            tweet.retweeted = false;
-                        }
+                response.instagram = [{
+                    "photos": photo_set_1
+                }, {
+                    "photos": photo_set_2
+                }, {
+                    "photos": photo_set_3
+                }, {
+                    "photos": photos
+                }];
 
-                        tweet.time_distance = moment(raw_tweet.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').calendar();
 
-                        tweets.push(tweet);
-                    });
 
-                    response.tweets = tweets;
 
-                    res.render('index', response);
-
-                });
 
 
             }
+
+            var T = new Twit({
+                consumer_key: config.twitter.consumer_key,
+                consumer_secret: config.twitter.consumer_secret,
+                access_token: config.twitter.access_token,
+                access_token_secret: config.twitter.access_token_secret
+            });
+
+
+            T.get('statuses/user_timeline', {
+                screen_name: config.screen_name,
+                count: 4
+            }, function(err, reply) {
+
+                var tweets = [];
+                _.each(reply, function(raw_tweet) {
+                    var tweet = {},
+                        hashtag_regexp = /#([a-zA-Z0-9]+)/g,
+                        username_regexp = /@([a-zA-Z0-9]+)/g;
+
+                    if (raw_tweet.retweeted_status) {
+                        tweet.img = raw_tweet.retweeted_status.user.profile_image_url;
+                        tweet.username = raw_tweet.retweeted_status.user.screen_name;
+                        tweet.tweet = raw_tweet.retweeted_status.text;
+                        tweet.retweeted = true;
+                    } else {
+                        tweet.username = raw_tweet.user.screen_name;
+                        tweet.img = raw_tweet.user.profile_image_url;
+                        tweet.tweet = raw_tweet.text;
+                        tweet.retweeted = false;
+                    }
+
+                    tweet.time_distance = moment(raw_tweet.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').calendar();
+
+                    tweet.tweet = tweet.tweet.replace(hashtag_regexp, '<span class="hashtag">#$1</span>');
+                    tweet.tweet = tweet.tweet.replace(username_regexp, '<span class="at-name">@$1</span>');
+
+                    tweets.push(tweet);
+                });
+
+                response.tweets = tweets;
+
+                res.render('index', response);
+
+            });
 
         });
 
