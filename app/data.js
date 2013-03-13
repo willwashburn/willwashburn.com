54363048b44d75266ba059f7a59157f5c5fa1839
Will Washburn <will@alfred>
2013-03-13 00:26:26 -0400

    simplfy data and update project template to reflect that simplification WERD

--- app/data.js
+++ app/data.js
@@ -77,15 +77,7 @@ module.exports = {
         'how': "HTML5, CSS, PHP, Javascript, PhoneGap, PHP websockets(!), Twitter Bootstrap, LESS, Tropo",
         'my_role': "My ‘founder scars’ come from Social Blendr. I put my blood, sweat and tears into this project; it was truly a life changing experience. I architected, designed, coded and was involved in every stage of the product. Mentored my co-founder as he learned to code.",
         'highlights': "We created 7 versions of a web app, a PHP websockets powered chat mobile application, a SMS check-in system, a HTML5 canvas drawing game,",
-        "thumbs": [{
-            "photo": 'sb-1.png'
-        }, {
-            "photo": 'sb-2.png'
-        }, {
-            "photo": 'sb-3.png'
-        }, {
-            "photo": 'sb-4.png'
-        }],
+        "thumbs": ['sb-1.png','sb-2.png','sb-3.png','sb-4.png'],
         "url": "www.socialblendr.com"
     }, {
         "project": 'connectedgreek',
