0c1a1cbc0cec2f0c264810e97e6d353b1b35391c
Will Washburn <will@alfred>
2013-03-13 13:24:35 -0400

    willigant blurb & minor updates

--- app/data.js
+++ app/data.js
@@ -74,7 +74,7 @@ module.exports = {
         "name": "Social Blendr",
         "has_logo": true,
         "what": "The best way to meet new people. We create connections with people online and get them together in real life.",
-        'how': "HTML5, CSS, PHP, Javascript, PhoneGap, PHP websockets(!), Twitter Bootstrap, LESS, Tropo",
+        'how': "HTML5, CSS, PHP, Javascript, Git, PhoneGap, Objective-C, PHP websockets(!), Twitter Bootstrap, LESS, Tropo, Linode VPS with centOS",
         'my_role': "My ‘founder scars’ come from Social Blendr. I put my blood, sweat and tears into this project; it was truly a life changing experience. I architected, designed, coded and was involved in every stage of the product. I also mentored my co-founder as he learned to code. Ultimately, we failed but I learned so much along the way it's not how I view it.",
         'highlights': "We created 7 versions of a web app, a PHP websockets powered chat mobile application, a SMS check-in system, a HTML5 canvas drawing game,",
         "thumbs": ['sb-1.png', 'sb-2.png', 'sb-4.png', 'sb-5.png'],
@@ -85,35 +85,40 @@ module.exports = {
         "has_logo": true,
         "thumbs": ['cg-1.png', 'cg-8.png', 'cg-5.png', 'cg-7.png'],
         "url": "www.connectedgreek.com",
-        "what":"Website and CMS for fraternities and sororities",
-        "how":"PHP, HTML, custom PHP MVC framework, custom javascript page editor and photo uploader, Adobe Fireworks",
-        "my_role":"It started as a website for my own fraternity and quickly became my full time job out of college. I earned my stripes on this project building my own framework and drafting the designs for each and every client. I also built a javascript page editor that allowed clients to update the webiste on their own.",
-        "highlights":"I was able to provide for more clients the more I automated processes. What started as 'client websites' quickly became a SASS as I started to love making myself more efficient through code. Building ConnectedGreek solidified my love for building software."
+        "what": "Website and CMS for fraternities and sororities",
+        "how": "PHP, HTML, custom PHP MVC framework, custom javascript page editor and photo uploader, Adobe Fireworks, Linode VPS with centoOS, Git",
+        "my_role": "It started as a website for my own fraternity and quickly became my full time job out of college. I earned my stripes on this project building my own framework and drafting the designs for each and every client. I also built a javascript page editor that allowed clients to update the webiste on their own.",
+        "highlights": "I was able to provide for more clients the more I automated processes. What started as 'client websites' quickly became a SASS as I started to love making myself more efficient through code. Building ConnectedGreek solidified my love for building software."
     }, {
         "project": 'willigant',
         "name": 'WilliGant',
         "thumbs": ['wg-2.png', 'wg-3.png', 'wg-6.png', 'wg-4.png'],
-        "has_logo": true
+        "has_logo": true,
+        "url": 'www.willigant.com',
+        "what": 'Website design',
+        "how": "HTML,CSS,Javascript,Adobe Fireworks, Google Apps",
+        "my_role": "Build websites and hope to get paid!",
+        "highlights": "It was rewarding helping people learn to leverage technology to better help their business. It was also a learning experience in invoicing."
     }, {
         "project": 'finewines',
         "name": 'Fine Wines',
-        "thumbs": ['fw-2.jpg', 'fw-3.jpg', 'fw-4.jpg','fw-1.png'],
+        "thumbs": ['fw-2.jpg', 'fw-3.jpg', 'fw-4.jpg', 'fw-1.png'],
         "has_logo": true,
-        "url":"finewines.willigant.com/Fitzpatrick/"
+        "url": "finewines.willigant.com/Fitzpatrick/"
     }, {
         "project": 'tugolearn',
         "name": 'Tug-O-Learn',
-        "thumbs": ['tug-3.jpg','tug-1.png','tug-2.png','tug-4.gif'],
+        "thumbs": ['tug-3.jpg', 'tug-1.png', 'tug-2.png', 'tug-4.gif'],
         "has_logo": true
     }, {
         "project": 'gvlater',
         "name": 'GV-Later',
-        "thumbs": ['gv-1.png','gv-2.png'],
+        "thumbs": ['gv-1.png', 'gv-2.png'],
         "has_logo": true
     }, {
         "project": 'thecampusbutler',
         "name": 'The Campus Butler',
-        "thumbs": ['cb-1.png','cb-2.png'],
+        "thumbs": ['cb-1.png', 'cb-2.png'],
         "has_logo": true
     }],
     "nerdraps": [
