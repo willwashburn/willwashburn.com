5c4c670c5e9078b8730d8d6aabc032fa587d1eb6
Will Washburn <will@alfred>
2013-03-13 12:31:36 -0400

    add images to projects

--- app/data.js
+++ app/data.js
@@ -77,26 +77,40 @@ module.exports = {
         'how': "HTML5, CSS, PHP, Javascript, PhoneGap, PHP websockets(!), Twitter Bootstrap, LESS, Tropo",
         'my_role': "My ‘founder scars’ come from Social Blendr. I put my blood, sweat and tears into this project; it was truly a life changing experience. I architected, designed, coded and was involved in every stage of the product. I also mentored my co-founder as he learned to code. Ultimately, we failed but I learned so much along the way it's not how I view it.",
         'highlights': "We created 7 versions of a web app, a PHP websockets powered chat mobile application, a SMS check-in system, a HTML5 canvas drawing game,",
-        "thumbs": ['sb-1.png','sb-2.png','sb-3.png','sb-4.png'],
+        "thumbs": ['sb-1.png', 'sb-2.png', 'sb-4.png', 'sb-5.png'],
         "url": "www.socialblendr.com"
     }, {
         "project": 'connectedgreek',
-        "name": 'Connected Greek'
+        "name": 'Connected Greek',
+        "has_logo": true,
+        "thumbs": ['cg-1.png', 'cg-8.png', 'cg-5.png', 'cg-7.png'],
+        "url": "www.connectedgreek.com"
     }, {
         "project": 'willigant',
-        "name": 'WilliGant'
+        "name": 'WilliGant',
+        "thumbs": ['wg-2.png', 'wg-3.png', 'wg-6.png', 'wg-4.png'],
+        "has_logo": true
     }, {
         "project": 'finewines',
-        "name": 'Fine Wines'
+        "name": 'Fine Wines',
+        "thumbs": ['fw-2.jpg', 'fw-3.jpg', 'fw-4.jpg','fw-1.png'],
+        "has_logo": true,
+        "url":"finewines.willigant.com/Fitzpatrick/"
     }, {
-        "project": 'tug-o-learn',
-        "name": 'Tug-O-Learn'
+        "project": 'tugolearn',
+        "name": 'Tug-O-Learn',
+        "thumbs": ['tug-3.jpg','tug-1.png','tug-2.png','tug-4.gif'],
+        "has_logo": true
     }, {
-        "project": 'gv-later',
-        "name": 'GV-Later'
+        "project": 'gvlater',
+        "name": 'GV-Later',
+        "thumbs": ['gv-1.png','gv-2.png'],
+        "has_logo": true
     }, {
         "project": 'thecampusbutler',
-        "name": 'The Campus Butler'
+        "name": 'The Campus Butler',
+        "thumbs": ['cb-1.png'],
+        "has_logo": true
     }],
     "nerdraps": [
 
