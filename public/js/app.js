880a5c2ba66b7bf61dcb16b25b4435335ced6e4c
Will Washburn <will@alfred>
2013-03-14 01:22:01 -0400

    some copy/pasta to get the nav working..cant wait to implement backbone

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,16 @@
 var ww = ww || {};
 
-
+ww.project_offset = 70;
 
 $('document').ready(function() {
-
 
     $('.sticky').waypoint(function(direction) {
 
         if (direction === 'down') {
 
-            var wrapper = '<div id="sticky-wrapper" style="width:' + $(this).width() + 'px; height:' + $(this).outerHeight(true) +'px">&nbsp;</div>';
+            var wrapper = '<div id="sticky-wrapper" style="width:' + $(this).width() + 'px; height:' + $(this).outerHeight(true) + 'px">&nbsp;</div>';
 
-            $(this).before(wrapper).css('position', 'fixed').css('top','0');
+            $(this).before(wrapper).css('position', 'fixed').css('top', '0');
 
             $(this).find('.nav').addClass('active');
 
@@ -33,7 +32,70 @@ $('document').ready(function() {
         offset: 50
     });
 
-}); ;/*var WillWashburn = new Marionette.Application();
+
+    $('.divider-socialblendr').waypoint(function(direction) {
+
+        toggleNav('socialblendr', direction, '');
+    }, {
+        offset: ww.project_offset
+    });
+
+    $('.divider-connectedgreek').waypoint(function(direction) {
+
+        toggleNav('connectedgreek', direction, 'socialblendr');
+    }, {
+        offset: ww.project_offset
+    });
+
+    $('.divider-finewines').waypoint(function(direction) {
+
+        toggleNav('finewines', direction, 'connectedgreek');
+    }, {
+        offset: ww.project_offset
+    });
+
+    $('.divider-willigant').waypoint(function(direction) {
+
+        toggleNav('willigant', direction, 'finewines');
+    }, {
+        offset: ww.project_offset
+    });
+
+    $('.divider-tugolearn').waypoint(function(direction) {
+
+        toggleNav('tugolearn', direction, 'willigant');
+    }, {
+        offset: ww.project_offset
+    });
+
+    $('.divider-gvlater').waypoint(function(direction) {
+
+        toggleNav('gvlater', direction, 'tugolearn');
+    }, {
+        offset: ww.project_offset
+    });
+
+    $('.divider-thecampusbutler').waypoint(function(direction) {
+
+        toggleNav('thecampusbutler', direction, 'gvlater');
+    }, {
+        offset: ww.project_offset
+        
+    });
+});
+
+
+var toggleNav = function(project, direction, above) {
+        $('.nav .active').removeClass('active');
+
+        if (direction === 'down') {
+            $('#nav-' + project).addClass('active');
+        } else if (direction === 'up') {
+            $('#nav-' + above).addClass('active');
+            $('#nav-' + project).removeClass('active');
+        }
+
+    };;/*var WillWashburn = new Marionette.Application();
 
 WillWashburn.addRegions({
 	header:'#header',
